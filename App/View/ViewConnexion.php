01fe897b9fb89f239c4f74e24b3f5e95213ae996
Tony3130 <ventonanthony@gmail.com>
2025-05-02 11:45:46 +0200

    change php

--- App/View/ViewConnexion.php
+++ App/View/ViewConnexion.php
@@ -1,13 +1,24 @@
 <!DOCTYPE html>
 <html lang="fr">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="./js/script.js" defer></script>
-    <link rel="stylesheet" href="./css/main.css">
-    <title>Mon profil</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="./public/assets/styles/main.css">
+  <script src="./public/assets/scripts/main.js" async></script>
+  <title>Connexion</title>
 </head>
 <body>
-    <h1>Bienvenue sur mon super site</h1>
+  <h1>Se connecter</h1>
+  <form action="" method="post">
+    <label for="email">Veuillez saisir votre adresse email :</label>
+    <input type="email" name="email" id="email">
+    
+    <label for="password">Veuillez saisir votre mot de passe :</label>
+    <input type="password" name="password" id="password">
+    
+    <input type="submit" name="submit" value="connexion">
+  </form>
 </body>
 </html>
+
+
